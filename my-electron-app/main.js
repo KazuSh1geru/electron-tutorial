80ba576dec6176306244f0380ce1ccad78f7e1e9
髙草木 和史 <kazushi.takakusagi@gmail.com>
2025-03-03 17:18:30 +0900

    overlay demo

--- my-electron-app/main.js
+++ my-electron-app/main.js
@@ -1,19 +1,63 @@
 // app, which controls your application's event lifecycle.
 // BrowserWindow, which creates and manages app windows.
 
-const { app, BrowserWindow } = require('electron/main')
+const { app, BrowserWindow, ipcMain, screen } = require('electron')
+const path = require('path')
+
+let mainWindow
+let overlayWindow
 
 const createWindow = () => {
-  const win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
   })
 
-  win.loadFile('index.html')
+  mainWindow.loadFile('index.html')
+}
+
+const createOverlayWindow = () => {
+  overlayWindow = new BrowserWindow({
+    width: 40,
+    height: 40,
+    frame: false,
+    transparent: true,
+    alwaysOnTop: true,
+    skipTaskbar: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
+
+  overlayWindow.loadFile('overlay.html')
+}
+
+// テスト用: マウスの位置にオーバーレイを表示
+const showOverlayAtMousePosition = () => {
+  const { getCursorScreenPoint } = screen
+  const point = getCursorScreenPoint()
+  
+  if (!overlayWindow) {
+    createOverlayWindow()
+  }
+  
+  overlayWindow.setPosition(point.x, point.y)
+  overlayWindow.show()
 }
 
 app.whenReady().then(() => {
   createWindow()
+
+  // テスト用のショートカットキーを登録
+  const { globalShortcut } = require('electron')
+  globalShortcut.register('CommandOrControl+Shift+T', () => {
+    showOverlayAtMousePosition()
+  })
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
@@ -25,5 +69,13 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
+  }
+})
+
+// オーバーレイがクリックされたときの処理
+ipcMain.on('overlay-clicked', () => {
+  console.log('Overlay clicked!')
+  if (overlayWindow) {
+    overlayWindow.hide()
   }
 })
