05112a9f6f49c2e52f5aa716b84a6be3058f4b93
髙草木 和史 <kazushi.takakusagi@gmail.com>
2025-03-03 16:26:39 +0900

    electron concept

--- my-electron-app/main.js
+++ my-electron-app/main.js
@@ -1,1 +1,17 @@
-console.log('Hello from Electron 👋')
+// app, which controls your application's event lifecycle.
+// BrowserWindow, which creates and manages app windows.
+
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
